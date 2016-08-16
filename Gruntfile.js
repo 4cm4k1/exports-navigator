c7e9fe44ef1f651a4a624449e9b9c70b0faf3147
Anthony Maki <4cm4k1@gmail.com>
2016-08-16 17:03:07 -0500

    Complete skeleton of project, including grunt copy, jshint, uglify, cssmin, and watch automation tools, and all anticipated npm/node dependencies, plus .gitignore and README.md

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        jshint: {
+            files: ['Gruntfile.js', 'client/scripts/*.js', 'routes/*.js', 'modules/*.js', 'server.js']
+        },
+        watch: {
+            files: ['client/**/*', 'server.js', 'modules/*.js', 'routes/*.js'],
+            tasks: ['jshint', 'uglify', 'cssmin']
+        },
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'node_modules',
+                    src: ['angular/**', 'angular-animate/**', 'angular-aria/**', 'angular-material/**', 'angular-messages/**', 'angular-route/**', 'angularfire/**', 'firebase/**'],
+                    dest: 'public/assets/vendors'
+                }, ],
+            },
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'public/assets/scripts/client.min.js': ['client/scripts/*.js']
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'client/styles',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'public/assets/styles',
+                    ext: '.min.css'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'watch']);
+
+};
---
+++ client/scripts/appModule.js
@@ -0,0 +1,6 @@
+(function(){
+    'use strict';
+
+    angular.module('exportsNavigator', ['ngMaterial', 'ngMessages', 'firebase', 'ngRoute']);
+
+})();
