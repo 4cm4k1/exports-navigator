d5d6896d767bce0add59a9309495cdc4aa14841c
Anthony Maki <4cm4k1@gmail.com>
2016-08-17 16:03:02 -0500

    Test

--- client/scripts/firebaseConfig.js
+++ client/scripts/firebaseConfig.js
@@ -1,9 +1,12 @@
 (function(){
+    'use strict';
+
     var config = {
         apiKey: "AIzaSyCEH70PEdLMF9KAuPecpohwOr0t6VQuubU",
         authDomain: "exports-navigator.firebaseapp.com",
         databaseURL: "https://exports-navigator.firebaseio.com",
         storageBucket: "exports-navigator.appspot.com",
     };
+
     firebase.initializeApp(config);
 })();
