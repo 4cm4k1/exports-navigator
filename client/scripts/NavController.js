949d896316e98c9d71e96934e9993b4f84c9e360
Liz Kerber <emkerber@outlook.com>
2016-08-17 17:31:29 -0500

    Began working on nav bar

---
+++ client/scripts/NavController.js
@@ -0,0 +1,11 @@
+(function() ***REMOVED***
+  'use strict';
+  angular.module('exportsNavigator').controller('NavController', navCtrl);
+
+  navCtrl.$inject = ['$location'];
+
+  function navCtrl($location) ***REMOVED***
+        var vm = this;
+  ***REMOVED***
+
+***REMOVED***)();
