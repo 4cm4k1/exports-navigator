bdd1a251381e358ea2ca21ed929b5927da3f4c93
Anthony Maki <4cm4k1@gmail.com>
2016-08-17 16:54:39 -0500

    Sign in functionality works; doesn't allow nonexistent users in

--- client/scripts/AuthCtrl.js
+++ client/scripts/AuthCtrl.js
@@ -1,13 +1,39 @@
-(function(){
+(function() {
     'use strict';
 
     angular.module('exportsNavigator').controller('AuthCtrl', AuthCtrl);
 
-    AuthCtrl.$inject = ['Auth'];
+    AuthCtrl.$inject = ['Auth', '$location'];
 
-    function AuthCtrl(Auth){
+    function AuthCtrl(Auth, $location) {
         var vm = this;
 
-        
+        //  This will redirect the user to the admin dashboard
+        //  if they already have a session going
+        Auth.$onAuthStateChanged(function(firebaseUser) {
+            if (firebaseUser) {
+                console.log('User is already signed in!');
+                $location.path('/admin');
+            }
+        });
+
+        // no need to redeclare, but for reference, the view is making this
+        // variable available to vm
+        // vm.user = {
+        //     email: 'something@example.com',
+        //     password: 'somethingelse'
+        // };
+
+        vm.signIn = function() {
+            Auth.$signInWithEmailAndPassword(vm.user.email, vm.user.password)
+                .then(function(firebaseUser) {
+                    console.log('Signed in as:', firebaseUser.uid);
+                    $location.path('/admin');
+                })
+                .catch(function(error) {
+                    console.error('Authentication failed:', error);
+                    // toast to indicate failure goes here
+                });
+        };
     }
 })();
