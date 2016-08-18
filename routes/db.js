30bc408e56870aee4bc839e1a5ca5789eca1042b
Katie Vogel <katiebvogel@gmail.com>
2016-08-18 15:51:37 -0500

    added SQL expres routes

--- routes/db.js
+++ routes/db.js
@@ -22,6 +22,48 @@ router.get('/contacts', function(req, res){
   });
 });
 
+
+//below is the router for getting all info based on the INDUSTRY SEARCH
+router.get('/industries', function(req, res){
+  pool.connect(function(err, client, done){
+    if(err) return res.send(err.code);
+    client.query('SELECT * FROM industries JOIN contacts ON (contacts.id = industries.contact_1 OR contacts.id = industries.contact_2 OR contacts.id = industries.contact_3) JOIN websites ON (websites.id = industries.website_1 OR websites.id = industries.website_2 OR websites.id = industries.website_3)', [], function(err, queryRes){
+      done();
+      if(err) return res.send(err);
+      res.send(queryRes.rows);
+    });
+  });
+}); //end the router.GET for INDUSTRY SEARCH
+
+
+//below is the route for getting all info based on the TOPIC SEARCH
+router.get('/topics', function(req, res){
+  pool.connect(function(err, client, done){
+    if(err) return res.send(err.code);
+    client.query('SELECT * FROM topics JOIN contacts ON (contacts.id = topics.contact_1 OR contacts.id = topics.contact_2 OR contacts.id = topics.contact_3) JOIN websites ON (websites.id = topics.website_1 OR websites.id = topics.website_2 OR websites.id = topics.website_3)', [], function(err, queryRes){
+      done();
+      if(err) return res.send(err);
+      res.send(queryRes.rows);
+    });
+  });
+}); //end the router.GET for TOPIC SEARCH
+
+
+
+//below is the route for getting all info based on the COUNTRY SEARCH
+router.get('/countries', function(req, res){
+  pool.connect(function(err, client, done){
+    if(err) return res.send(err.code);
+    client.query('SELECT * FROM countries JOIN contacts ON contacts.id = countries.contact_id', [], function(err, queryRes){
+      done();
+      if(err) return res.send(err);
+      res.send(queryRes.rows);
+    });
+  });
+}); //end the router.GET for COUNTRY SEARCH
+
+
+
 pool.on('error', function (err, client) {
   // if an error is encountered by a client while it sits idle in the pool
   // the pool itself will emit an error event with both the error and
