ce97dbaec72caefa8d58b5a303555a41be451af5
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-08-18 10:14:34 -0500

    initial

---
+++ routes/db.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var pg = require('pg');
+require('dotenv').config()
+
+pg.defaults.ssl = true;
+
+router.get('/contacts', function(req, res){
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
+    client
+      .query('SELECT * FROM contacts')
+      .on('row', function(row) {
+        console.log(JSON.stringify(row));
+      });
+  });
+});
+
+module.exports = router;
