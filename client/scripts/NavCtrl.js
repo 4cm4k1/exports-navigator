3cda42b664c988e8f2245688607f0d17b3f9974f
Anthony Maki <4cm4k1@gmail.com>
2016-08-18 15:10:39 -0500

    Nav bar now does not show unless admin user is logged in

--- client/scripts/NavCtrl.js
+++ client/scripts/NavCtrl.js
@@ -3,11 +3,17 @@
 
     angular.module('exportsNavigator').controller('NavCtrl', NavCtrl);
 
-    NavCtrl.$inject = ['$location'];
+    NavCtrl.$inject = ['$location', 'Auth'];
 
-    function NavCtrl($location) ***REMOVED***
+    function NavCtrl($location, Auth) ***REMOVED***
         var vm = this;
 
+        vm.auth = Auth;
+
+        vm.auth.$onAuthStateChanged(function(user)***REMOVED***
+            vm.user = user;
+            console.log('navbar ctrl:', vm.user);
+        ***REMOVED***);
 
     ***REMOVED***;
 
