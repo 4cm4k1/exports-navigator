206b37b3ad4885ca8522000a1b4ee446eb8112d7
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-08-18 10:58:06 -0500

    express routes

--- routes/db.js
+++ routes/db.js
@@ -2,19 +2,34 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var pg = require('pg');
+var connectionString = process.env.DATABASE_URL;
 
 pg.defaults.ssl = true;
+var pool = new pg.Pool(connectionString);
 
 router.get('/contacts', function(req, res)***REMOVED***
-  pg.connect(process.env.DATABASE_URL, function(err, client) ***REMOVED***
-    if (err) throw err;
-    console.log('Connected to postgres! Getting schemas...');
-    client
-      .query('SELECT * FROM contacts')
-      .on('row', function(row) ***REMOVED***
-        console.log(JSON.stringify(row));
-      ***REMOVED***);
-  ***REMOVED***);
+  pool.connect(err, client, done)***REMOVED***
+    if(err) ***REMOVED***
+    res.send('Error fetching client from pool', err);
+    ***REMOVED***
+    client.query('SELECT * FROM contacts',[], function(err, result)***REMOVED***
+      done();
+      if(err) ***REMOVED***
+      res.send('error running query', err);
+      ***REMOVED***
+      res.send(result);
+    ***REMOVED***);
+  ***REMOVED***;
 ***REMOVED***);
+
+pool.on('error', function (err, client) ***REMOVED***
+  // if an error is encountered by a client while it sits idle in the pool
+  // the pool itself will emit an error event with both the error and
+  // the client which emitted the original error
+  // this is a rare occurrence but can happen if there is a network partition
+  // between your application and the database, the database restarts, etc.
+  // and so you might want to handle it and at least log it out
+  console.error('idle client error', err.message, err.stack)
+***REMOVED***)
 
 module.exports = router;
