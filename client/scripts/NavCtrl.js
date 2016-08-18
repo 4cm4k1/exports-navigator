4a50fa199b162c7098daa435084671c758b5750b
Anthony Maki <4cm4k1@gmail.com>
2016-08-18 10:16:14 -0500

    Change NavController to NavCtrl - was causing jshint to error out

---
+++ client/scripts/NavCtrl.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular.module('exportsNavigator').controller('NavCtrl', NavCtrl);
+
+    NavCtrl.$inject = ['$location'];
+
+    function NavCtrl($location) {
+        var vm = this;
+    }
+
+})();
