e8eaf75c6b1454ebe790799d2ec72e10b7aaf345
Anthony Maki <4cm4k1@gmail.com>
2016-08-18 14:16:21 -0500

    Fix order in grunt uglification, edit admin view

--- client/scripts/configRouteTheme.js
+++ client/scripts/configRouteTheme.js
@@ -10,17 +10,17 @@
             controller: 'HomeCtrl as home',
             templateUrl: 'views/home.html'
         ***REMOVED***)
-        // .when('/admin', ***REMOVED***
-        //   controller: 'AdminHomeController as admin',
-        //   templateUrl: 'views/adminHome.html',
-        //   resolve: ***REMOVED***
-        //     //  This will require the user is logged in
-        //     //  and pass the current session info to the controller
-        //     'currentAuth': ['Auth', function(Auth)***REMOVED***
-        //       return Auth.$requireSignIn();
-        //     ***REMOVED***]
-        //   ***REMOVED***
-        // ***REMOVED***)
+        .when('/admin', ***REMOVED***
+          controller: 'AdminHomeController as admin',
+          templateUrl: 'views/adminHome.html',
+          resolve: ***REMOVED***
+            //  This will require the user is logged in
+            //  and pass the current session info to the controller
+            'currentAuth': ['Auth', function(Auth)***REMOVED***
+              return Auth.$requireSignIn();
+            ***REMOVED***]
+          ***REMOVED***
+        ***REMOVED***)
         .otherwise(***REMOVED***
           controller: 'HomeCtrl as home',
           templateUrl: 'views/home.html'
@@ -30,9 +30,34 @@
         $locationProvider.html5Mode(true);
 
         //  Angular Material theme config
-        $mdThemingProvider.theme('default')
-            .primaryPalette('teal')
-            .accentPalette('light-green')
-            .warnPalette('deep-orange');
+        $mdThemingProvider
+        .definePalette('GreaterMSPColors', ***REMOVED***
+          '50': '89ccda',   //  Greater MSP Blue
+          '100': 'd0d741',  //  Greater MSP Green
+          '200': '3a3431',  //  Greater MSP Brown - headline and body copy, rules, accents
+          '300': 'b7be16',  //  Body copy, rules, accents
+          '400': 'ece8b2',  //  Background tint
+          '500': '4fbbd1',  //  Body copy, rules, accents
+          '600': 'f5d258',  //  Comparative colors
+          '700': '98579b',
+          '800': 'e58426',
+          '900': 'cc6ca6',
+          'A100': '006da3',
+          'A200': 'b4981d',
+          'A400': 'db3762',
+          'A700': '006da3',
+          'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                              // on this palette should be dark or light
+          'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+           '200', '300', '400', 'A100'],
+          'contrastLightColors': undefined    // could also specify this if default was 'dark'
+
+        ***REMOVED***);
+        $mdThemingProvider
+        .theme('default')
+        .primaryPalette('GreaterMSPColors')
+        .accentPalette('GreaterMSPColors')
+        .warnPalette('GreaterMSPColors')
+        .backgroundPalette('grey');
     ***REMOVED***
 ***REMOVED***)();
