537e03106b04505965a76cec75c6456564053da6
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-08-18 14:33:55 -0500

    database config

--- routes/db.js
+++ routes/db.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var pg = require('pg');
-var connectionString = process.env.DATABASE_URL;
-
-pg.defaults.ssl = true;
-var pool = new pg.Pool(connectionString);
+var Pool = require('pg').Pool;
+//securely access heroku postgres configuration
+require('dotenv').config();
+var config = ***REMOVED***
+  host: process.env.DATABASE_HOST, //env var
+  user: process.env.DATABASE_USER, //env var
+  database: process.env.DATABASE_DB, //env var
+  password: process.env.DATABASE_PW, //env var
+  port: process.env.DATABASE_PORT, //env var
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  ssl: true
+***REMOVED***;
+var pool = new Pool(config);
 
 router.get('/contacts', function(req, res)***REMOVED***
-
+  pool.connect(function(err, client, done)***REMOVED***
+    if(err) return res.send(err.code);
+    client.query('SELECT * FROM contacts', [], function(err, queryRes)***REMOVED***
+      done();
+      if(err) return res.send(err);
+      res.send(queryRes);
+    ***REMOVED***);
+  ***REMOVED***);
 ***REMOVED***);
 
 pool.on('error', function (err, client) ***REMOVED***
