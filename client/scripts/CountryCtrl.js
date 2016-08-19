bbbb9aac1e4e240080e2a98c96525e72b37a825f
Sahasha Reese <codeHer@icloud.com>
2016-08-19 17:00:38 -0500

    Deleted unnecessary client side controllers and updated the four existing controllers with the appropriate names.

---
+++ client/scripts/CountryCtrl.js
@@ -0,0 +1,13 @@
+(function() ***REMOVED***
+    'use strict';
+
+    angular.module('exportsNavigator').controller('CountryCtrl', CountryCtrl);
+
+    CountryCtrl.$inject = [];
+
+    function CountryCtrl(Country) ***REMOVED***
+        var vm = this;
+
+
+    ***REMOVED***
+***REMOVED***)();
