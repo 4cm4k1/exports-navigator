6517e3e3bbaff18105191d52e102eaa0e1812a30
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-08-19 11:27:45 -0500

    fixed route function

--- routes/db.js
+++ routes/db.js
@@ -15,12 +15,12 @@ var pool = new Pool(config);
 //routes to return each table and related foreign key data KRQ
 router.get('/contacts', function(req, res){
   var query = 'SELECT * FROM contacts';
-  queryDB(query, []);
+  queryDB(query, [], req, res);
 });
 router.get('/countries', function(req, res){
   var query = 'SELECT * FROM countries JOIN contacts ON' +
   'contacts.id = countries.contact_id';
-  queryDB(query, []);
+  queryDB(query, [], req, res);
 });
 router.get('/industries', function(req, res){
   var query = 'SELECT * FROM industries JOIN contacts ON' +
@@ -28,16 +28,16 @@ router.get('/industries', function(req, res){
   'contacts.id = industries.contact_3) JOIN websites ON' +
   '(websites.id = industries.website_1 OR websites.id = industries.website_2 OR' +
   'websites.id = industries.website_3)';
-  queryDB(query, []);
+  queryDB(query, [], req, res);
 });
 router.get('/topics', function(req, res){
   var query = 'SELECT * FROM topics JOIN contacts ON' +
   '(contacts.id = topics.contact_1 OR contacts.id = topics.contact_2 OR contacts.id = topics.contact_3)' +
   'JOIN websites ON (websites.id = topics.website_1 OR websites.id = topics.website_2 OR websites.id = topics.website_3)';
-  queryDB(query, []);
+  queryDB(query, [], req, res);
 });
 //refactored routes to use one function for retrieving or sending data KRQ
-function queryDB(queryStatement, vars){
+function queryDB(queryStatement, vars, req, res){
   pool.connect(function(err, client, done){
     if(err) return res.send(err.code);
     client.query(queryStatement, vars, function(err, queryRes){
