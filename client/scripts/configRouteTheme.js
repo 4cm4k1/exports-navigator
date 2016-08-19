2aca0f83241da6788d43306a960c981f8df1f6ad
Anthony Maki <4cm4k1@gmail.com>
2016-08-19 16:16:17 -0500

    Condense public user routes to /results/:category/:searchTerm, add results view and controller

--- client/scripts/configRouteTheme.js
+++ client/scripts/configRouteTheme.js
@@ -6,12 +6,20 @@
     function configRouteTheme($routeProvider, $locationProvider, $mdThemingProvider) ***REMOVED***
         //  Route config (with authentication checks)
         $routeProvider
-        // route for the User Landing Page View
+
+        //  public landing view
         .when('/', ***REMOVED***
             controller: 'LandingCtrl as landing',
             templateUrl: 'views/landingPage.html'
         ***REMOVED***)
-        // route for the Admin Home Page
+
+        //  results view with $routeParams
+        .when('/results/:category/:searchTerm', ***REMOVED***
+          controller: 'ResultsCtrl as results',
+          templateUrl: 'views/results.html'
+        ***REMOVED***)
+
+        //  admin routes and subroutes views
         .when('/admin', ***REMOVED***
           controller: 'AdminHomeController as adminHome',
           templateUrl: 'views/adminHome.html',
@@ -51,35 +59,10 @@
           ***REMOVED***
         ***REMOVED***)
 
-        // route for the User Food-Aggribusiness View
-        .when('/user/food-aggribusiness', ***REMOVED***
-            controller: 'FoodAgCtrl as foodAg',
-            templateUrl: 'views/foodAg.html'
-        ***REMOVED***)
-        // route for the User Medical Device, Pharma, Life Sciences View
-        .when('/user/meddev_pharma_lifescience', ***REMOVED***
-            controller: 'MedDevCtrl as medDev',
-            templateUrl: 'views/medDev.html'
-        ***REMOVED***)
-        // route for the User Other View
-        .when('/user/other', ***REMOVED***
-            controller: 'OtherCtrl as other',
-            templateUrl: 'views/other.html'
-        ***REMOVED***)
-        // route for the User General Topic Search View
-        .when('/user/topic-search', ***REMOVED***
-            controller: 'TSearchCtrl as topics',
-            templateUrl: 'views/topicSearch.html'
-        ***REMOVED***)
-        // route for the User Country Search View
-        .when('/user/country-search', ***REMOVED***
-            controller: 'CSearchCtrl as country',
-            templateUrl: 'views/countrySearch.html'
-        ***REMOVED***)
-        // route for the User Home Page View
+        //  catch-all view
         .otherwise(***REMOVED***
-          controller: 'HomeCtrl as home',
-          templateUrl: 'views/home.html'
+              controller: 'LandingCtrl as landing',
+              templateUrl: 'views/landingPage.html'
         ***REMOVED***);
 
         //  $locationProvider config
