d92173200a8947a89cc5bb9ffe69958776c37f5a
Liz Kerber <emkerber@outlook.com>
2016-08-19 16:40:14 -0500

    Working on displaying data on adminTopics

--- client/scripts/AdminTopicsController.js
+++ client/scripts/AdminTopicsController.js
@@ -10,13 +10,15 @@
 
     $http.get('/db/topics').then(function(response) {
 
-      var list = [];
+      // var list = [];
 
-      for (var i = 0; i < response.data.rows.length; i++) {
-        list.push(response.data.rows[i].topic);
-      }
+      // for (var i = 0; i < response.data.rows.length; i++) {
+      //   list.push(response.data.rows[i].topic);
+      // }
 
-      console.log('list variable:', list);
+      // console.log('list variable:', list);
+
+      console.log(response);
     });
 
     vm.topicsList = ['this', 'will', 'be', 'a', 'list', 'of', 'topics'];
