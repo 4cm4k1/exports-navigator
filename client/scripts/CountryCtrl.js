e5c9578ab81db2d34fd9c2a90c3d53bb2332fb4c
Sahasha Reese <codeHer@icloud.com>
2016-08-22 14:21:14 -0500

    Comment out CTRL's still updating.

--- client/scripts/CountryCtrl.js
+++ client/scripts/CountryCtrl.js
@@ -1,36 +1,36 @@
-//client/scripts/CountryCtrl.js User Country View Controller
-(function() ***REMOVED***
-    'use strict';
-
-    angular.module('exportsNavigator').controller('CountryCtrl', CountryCtrl);
-    CountryCtrl.$inject = ['$state', 'countryService', 'countries', 'country'];
-
-    function CountryCtrl(Country) ***REMOVED***
-        var vm = this;
-
-
-    ***REMOVED***
-***REMOVED***)();
-
-    function countryData($state, countryService, countries, country) ***REMOVED***
-
-    this.country = country.data;
-
-    this.countryQuery = $state.params.query;
-
-    this.country = countries.data;
-
-    this.saveCountry = function() ***REMOVED***
-        countryService.saveCountry(this.country)
-            .then(function() ***REMOVED***
-                $state.go('countries');
-            ***REMOVED***);
-    ***REMOVED***;
-
-    this.searchCountries = function(query) ***REMOVED***
-        if (!query.length) return $state.go('countries');
-
-        $state.go('search', ***REMOVED***query: query***REMOVED***);
-    ***REMOVED***;
-
-***REMOVED***
+// //client/scripts/CountryCtrl.js User Country View Controller
+// (function() ***REMOVED***
+//     'use strict';
+//
+//     angular.module('exportsNavigator').controller('CountryCtrl', CountryCtrl);
+//     CountryCtrl.$inject = ['$state', 'countryService', 'countries', 'country'];
+//
+//     function CountryCtrl(Country) ***REMOVED***
+//         var vm = this;
+//
+//
+//     ***REMOVED***
+// ***REMOVED***)();
+//
+//     function countryData($state, countryService, countries, country) ***REMOVED***
+//
+//     this.country = country.data;
+//
+//     this.countryQuery = $state.params.query;
+//
+//     this.country = countries.data;
+//
+//     this.saveCountry = function() ***REMOVED***
+//         countryService.saveCountry(this.country)
+//             .then(function() ***REMOVED***
+//                 $state.go('countries');
+//             ***REMOVED***);
+//     ***REMOVED***;
+//
+//     this.searchCountries = function(query) ***REMOVED***
+//         if (!query.length) return $state.go('countries');
+//
+//         $state.go('search', ***REMOVED***query: query***REMOVED***);
+//     ***REMOVED***;
+//
+// ***REMOVED***
