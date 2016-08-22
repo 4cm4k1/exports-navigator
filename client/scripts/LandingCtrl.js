d61ff7bc320106b6c22fda0c05b297db6b7f9f6c
Sahasha Reese <codeHer@icloud.com>
2016-08-22 14:21:14 -0500

    Comment out CTRL's still updating.

--- client/scripts/LandingCtrl.js
+++ client/scripts/LandingCtrl.js
@@ -1,26 +1,26 @@
-//client/scripts/LandingCtrl.js User Landing View Controller
-(function() ***REMOVED***
-    'use strict';
-
-    angular.module('exportsNavigator').controller('LandingCtrl', LandingCtrl);
-
-    function LandingCtrl($scope, $http) ***REMOVED***
-        $scope.prequalData = [***REMOVED***
-            label: 'Food-Aggribusiness',
-            value: 'Food-Aggribusiness'
-        ***REMOVED***, ***REMOVED***
-            label: 'Medical Device, Pharma & Life Sciences',
-            value: 'Medical Device, Pharma & Life Sciences'
-        ***REMOVED***, ***REMOVED***
-            label: 'Other',
-            value: 'Other',
-            /*isDisabled: true*/
-        ***REMOVED***, ];
-        $scope.group = 'Food-Aggribusiness';
-
-        function LandingCtrl(Landing) ***REMOVED***
-            var vm = this;
-        ***REMOVED***
-
-    ***REMOVED***
-***REMOVED***)();
+// //client/scripts/LandingCtrl.js User Landing View Controller
+// (function() ***REMOVED***
+//     'use strict';
+//
+//     angular.module('exportsNavigator').controller('LandingCtrl', LandingCtrl);
+//
+//     function LandingCtrl($scope, $http) ***REMOVED***
+//         $scope.prequalData = [***REMOVED***
+//             label: 'Food-Aggribusiness',
+//             value: 'Food-Aggribusiness'
+//         ***REMOVED***, ***REMOVED***
+//             label: 'Medical Device, Pharma & Life Sciences',
+//             value: 'Medical Device, Pharma & Life Sciences'
+//         ***REMOVED***, ***REMOVED***
+//             label: 'Other',
+//             value: 'Other',
+//             /*isDisabled: true*/
+//         ***REMOVED***, ];
+//         $scope.group = 'Food-Aggribusiness';
+//
+//         function LandingCtrl(Landing) ***REMOVED***
+//             var vm = this;
+//         ***REMOVED***
+//
+//     ***REMOVED***
+// ***REMOVED***)();
--- client/scripts/TopicsCtrl.js
+++ client/scripts/TopicsCtrl.js
@@ -7,8 +7,7 @@
 
     function TopicsCtrl(Topic) ***REMOVED***
         var vm = this;
-
-
+        console.log('Injeting some stuff.');
     ***REMOVED***
 ***REMOVED***)();
 
@@ -24,6 +23,7 @@
         topicsService.saveTopics(this.topic)
             .then(function() ***REMOVED***
                 $state.go('topics');
+                console.log('Test Topics Save.');
             ***REMOVED***);
     ***REMOVED***;
 
@@ -31,6 +31,7 @@
         if (!query.length) return $state.go('topics');
 
         $state.go('search', ***REMOVED***query: query***REMOVED***);
+        console.log('Searching Topics!');
     ***REMOVED***;
 
 ***REMOVED***
