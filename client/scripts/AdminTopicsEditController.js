f7bc1ec4857c0e4bdef3d71eb0c1fc72d2175ac3
Liz Kerber <emkerber@outlook.com>
2016-08-22 16:06:37 -0500

    Pretty-printed admin topic edit view's data

--- client/scripts/AdminTopicsEditController.js
+++ client/scripts/AdminTopicsEditController.js
@@ -26,5 +26,12 @@
       // console.log('list at index:', list[index]);
     });
 
+  vm.update = function() {
+    // $http.put('/db/topics/update').then(function(request, response) {
+    //
+    // });
+    console.log(vm.)
+  };
+
   }
 })();
