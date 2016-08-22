13b31fabcfe9b145906e7895e71352e8023d47d8
Sahasha Reese <codeHer@icloud.com>
2016-08-22 14:21:14 -0500

    Comment out CTRL's still updating.

--- client/scripts/IndustryCtrl.js
+++ client/scripts/IndustryCtrl.js
@@ -1,36 +1,36 @@
-//client/scripts/IndustryCtrl.js User Industry View Controller
-(function() {
-    'use strict';
-
-    angular.module('exportsNavigator').controller('IndustryCtrl', IndustryCtrl);
-    IndustryCtrl.$inject = ['$state', 'industriesService', 'industries', 'industry'];
-
-    function IndustryCtrl(Industry) {
-        var vm = this;
-
-
-    }
-})();
-
-    function industryData($state, industriesService, industries, industry) {
-
-    this.industry = industry.data;
-
-    this.industryQuery = $state.params.query;
-
-    this.industries = industries.data;
-
-    this.saveIndustry = function() {
-        industriesService.saveIndustry(this.industry)
-            .then(function() {
-                $state.go('industries');
-            });
-    };
-
-    this.searchIndustries = function(query) {
-        if (!query.length) return $state.go('industries');
-
-        $state.go('search', {query: query});
-    };
-
-}
+// //client/scripts/IndustryCtrl.js User Industry View Controller
+// (function() {
+//     'use strict';
+//
+//     angular.module('exportsNavigator').controller('IndustryCtrl', IndustryCtrl);
+//     IndustryCtrl.$inject = ['$state', 'industriesService', 'industries', 'industry'];
+//
+//     function IndustryCtrl(Industry) {
+//         var vm = this;
+//
+//
+//     }
+// })();
+//
+//     function industryData($state, industriesService, industries, industry) {
+//
+//     this.industry = industry.data;
+//
+//     this.industryQuery = $state.params.query;
+//
+//     this.industries = industries.data;
+//
+//     this.saveIndustry = function() {
+//         industriesService.saveIndustry(this.industry)
+//             .then(function() {
+//                 $state.go('industries');
+//             });
+//     };
+//
+//     this.searchIndustries = function(query) {
+//         if (!query.length) return $state.go('industries');
+//
+//         $state.go('search', {query: query});
+//     };
+//
+// }
