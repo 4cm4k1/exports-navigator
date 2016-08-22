4ff79f048b908702af3b3cb9465e155b0968165d
Katie Vogel <katiebvogel@gmail.com>
2016-08-22 12:07:59 -0500

    added two routes for unmatched topics

--- routes/db.js
+++ routes/db.js
@@ -149,6 +149,23 @@ router.delete('/topics/delete', function(req, res){
 });
 
 
+//  ~ Admin Reports
+//below are routes for ADMIN REPORTS PURPOSES
+router.get('/unmatched', function(req, res){
+  var query = 'SELECT * FROM unmatched_topics';
+  queryDB(query, [], req, res);
+});
+
+router.post('/unmatched/create', function(req, res){
+  var query = 'INSERT INTO unmatched_topics' +
+'(unmatched_topic) VALUES' +
+'($1)';
+var params = [req.body.unmatched_topic];
+  queryDB(query, params, req, res);
+});
+
+
+
 
 
 //refactored routes to use one function for retrieving or sending data KRQ
