9f37a4298b3cd71ccaba5a2565e46f3b5e6ee85f
Anthony Maki <4cm4k1@gmail.com>
2016-08-23 11:21:18 -0500

    Add basic Data.js Angular factory

---
+++ client/scripts/Data.js
@@ -0,0 +1,11 @@
+(function(){
+    'use strict';
+
+    angular.module('exportsNavigator').factory('Data', ['$http', Data]);
+
+    function Data($http){
+        //  This is the basic framework for our data factory
+
+    }
+
+})();
