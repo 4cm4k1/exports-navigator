01fe8e92de09c514325899fe469123c4c80bbd25
Anthony Maki <4cm4k1@gmail.com>
2016-08-23 16:27:09 -0500

    Make some test methods in data factories

--- client/scripts/Data.js
+++ client/scripts/Data.js
@@ -5,7 +5,19 @@
 
     function Data($http){
         //  This is the basic framework for our data factory
+        this.aTestOfSomethingGreat = function(){
+            return 'Yay!';
+        };
 
+        this.heresAnotherGreatThing = function(){
+            return 'Eureka!';
+        };
+
+        //  Possibly group $http calls by /path ???
+
+        //  Return it all
+
+        return this;
     }
 
 })();
