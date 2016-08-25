79e01a91d53d7e2c9674ac382646d394c7047aee
Katie Vogel <katiebvogel@gmail.com>
2016-08-25 15:53:58 -0500

    results.html and controller with some functionality

--- client/scripts/ResultsCtrl.js
+++ client/scripts/ResultsCtrl.js
@@ -3,10 +3,27 @@
 
     angular.module('exportsNavigator').controller('ResultsCtrl', ResultsCtrl);
 
-    ResultsCtrl.$inject = ['$location', '$routeParams'];
+    ResultsCtrl.$inject = ['$location', '$routeParams', '$http'];
 
-    function ResultsCtrl($location, $routeParams){
+    function ResultsCtrl($location, $routeParams, $http){
         var vm = this;
+        console.log('category:', $routeParams.category);
+        console.log('search term:', $routeParams.searchTerm);
+
+
+        vm.getIndustryData = function() {
+            $http.get('/db/industries').then(function(response) {
+                    console.log('getting industries', response);
+                    vm.industryList = response.data.rows;
+                    return (vm.industryList);
+                },
+                function(response) {
+                    console.log('error getting industry data', response);
+                });
+
+        };
+        vm.getIndustryData();
+
 
         //  $routeParams.category & $routeParams.searchTerm will be made
         //  available to this controller
