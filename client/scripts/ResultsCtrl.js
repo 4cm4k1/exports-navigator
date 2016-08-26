b5bb7157a0f536b410d517bb1ca6024574b4a312
Katie Vogel <katiebvogel@gmail.com>
2016-08-26 16:25:26 -0500

    only results for selected industry are displayed in results.html

--- client/scripts/ResultsCtrl.js
+++ client/scripts/ResultsCtrl.js
@@ -9,30 +9,46 @@
         var vm = this;
         console.log('industry:', $routeParams.industry);
         console.log('topic (optional):', $routeParams.topic);
-
-
-
-        var index = $routeParams.industry;
-        index = parseInt(index);
-
-
-
+        vm.industry = $routeParams.industry;
+        vm.topic = $routeParams.topic;
+        //
+        // var id = $routeParams.industry;
+        // id = parseInt(id);
 
 
         vm.getIndustryData = function() {
+          var indId = $routeParams.industry;
+          indId = parseInt(indId);
+          console.log('industries', indId);
             $http.get('/db/industries').then(function(response) {
-                    console.log('getting industries', response);
+                    console.log('getting industries', response.data);
                     vm.industryList = response.data.rows;
                     return (vm.industryList);
                 },
                 function(response) {
-                    console.log('error getting industry data', response);
+                    console.log('error getting industry data', response.data);
                 });
-
         };
+
         vm.getIndustryData();
 
 
+
+//this is to get the list of Topics which should display only when someone has selected "other industries"
+        vm.getTopicList = function() {
+          var topicId = $routeParams.topic;
+          topicId = parseInt(topicId);
+          console.log('topics', topicId);
+            $http.get('/db/topics').then(function(response) {
+                    console.log('getting topics', response.data);
+                    vm.topicList = response.data.rows;
+                    return (vm.topicList);
+                },
+                function(response) {
+                    console.log('error getting topic data', response.data);
+                });
+        };
+        vm.getTopicList();
 
 
 
