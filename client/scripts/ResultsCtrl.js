e9b06eae939c0b56e7464f48115c3e58d0519b5e
Katie Vogel <katiebvogel@gmail.com>
2016-08-26 17:32:38 -0500

    added an autocomplete field for topic search. Not functional yet

--- client/scripts/ResultsCtrl.js
+++ client/scripts/ResultsCtrl.js
@@ -1,25 +1,22 @@
-(function(){
+(function() {
     'use strict';
 
     angular.module('exportsNavigator').controller('ResultsCtrl', ResultsCtrl);
 
     ResultsCtrl.$inject = ['$location', '$routeParams', '$http'];
 
-    function ResultsCtrl($location, $routeParams, $http){
+    function ResultsCtrl($location, $routeParams, $http) {
         var vm = this;
         console.log('industry:', $routeParams.industry);
         console.log('topic (optional):', $routeParams.topic);
         vm.industry = $routeParams.industry;
         vm.topic = $routeParams.topic;
-        //
-        // var id = $routeParams.industry;
-        // id = parseInt(id);
 
 
         vm.getIndustryData = function() {
-          var indId = $routeParams.industry;
-          indId = parseInt(indId);
-          console.log('industries', indId);
+            var indId = $routeParams.industry;
+            indId = parseInt(indId);
+            console.log('industries', indId);
             $http.get('/db/industries').then(function(response) {
                     console.log('getting industries', response.data);
                     vm.industryList = response.data.rows;
@@ -33,12 +30,11 @@
         vm.getIndustryData();
 
 
-
-//this is to get the list of Topics which should display only when someone has selected "other industries"
+        //this is to get the list of Topics which should display only when someone has selected "other industries"
         vm.getTopicList = function() {
-          var topicId = $routeParams.topic;
-          topicId = parseInt(topicId);
-          console.log('topics', topicId);
+            var topicId = $routeParams.topic;
+            topicId = parseInt(topicId);
+            console.log('topics', topicId);
             $http.get('/db/topics').then(function(response) {
                     console.log('getting topics', response.data);
                     vm.topicList = response.data.rows;
@@ -47,13 +43,56 @@
                 function(response) {
                     console.log('error getting topic data', response.data);
                 });
+                  return (vm.topicList);
         };
-        vm.getTopicList();
+        // vm.getTopicList();
+
+
+
+//all code below is under construction and related to the search by topics autocomplete field
+//I am thinking it will be part of an ng-hide/ng-show
+//if someone selects "other industries", then the radio buttons will be hidden and this
+//"search by topics" field will appear. 
+
+        vm.selectedItem = null;
+        vm.searchText = null;
+
+
+        // function querySearch(query) {
+        //     var results = query ? vm.topicList.filter(createFilterFor(query)) : vm.topicList;
+        //     // var deferred = $q.defer();
+        //     //  $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+        //     //  return deferred.promise;
+        // }
+
+        function loadAll() {
+            vm.getTopicList();
+            return allStates.split(/, +/g).map( function (state) {
+        return {
+          value: state.toLowerCase(),
+          display: state
+        };
+      });
+            return {
+            value: vm.topicList.topic.toLowerCase(),
+            display: topic
+          };
+        }
+
+        function createFilterFor(query) {
+            var lowercaseQuery = angular.lowercase(query);
+            return function filterFn(topic) {
+                return (topic.value.indexOf(lowercaseQuery) === 0);
+            };
+        }
 
 
 
 
-  // this code needs to go into the function that fires when someone chooses a topic
+
+
+
+        // this code needs to go into the function that fires when someone chooses a topic
 
 
         //
