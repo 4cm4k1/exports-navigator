ced4b03a4664fa375b0794b361fde5118d01f081
Anthony Maki <4cm4k1@gmail.com>
2016-08-29 12:14:14 -0500

    Export each object and function of Data.js individually inside of an object literal

--- client/scripts/Data.js
+++ client/scripts/Data.js
@@ -1,269 +1,253 @@
-(function(){
+(function() {
     'use strict';
 
-    angular.module('exportsNavigator').factory('Data', ['$http', '$firebaseAuth', Data]);
+    angular.module('exportsNavigator').factory('Data', ['$http', 'Auth', Data]);
 
-    function Data($http, $firebaseAuth){
+    function Data($http, Auth) {
 
-      /*
-       *  DATA OBJECT, TO WHICH THE BELOW FUNCTIONS WILL ADD KEYS FOR EACH SET
-       *  OF ROWS FROM THE DATABASE (EXPORTED)
-       *
-       */
+        /*
+         *  SYNCHRONIZED DATA OBJECT
+         */
 
-      // this.data = {};
+        var data = {};
 
-var data = {};
-      /*
-       *  START OF GET * CALLS
-       *  START OF PUBLIC
-       *
-       */
+        /*
+         *  GET CALLS
+         */
 
-       var getIndustries = function() {
+        var getIndustries = function() {
             $http.get('/db/industries')
-              .then(function(response){
-                console.log('inside get call', response.data.rows);
-                  data.industries = response.data.rows;
-              })
-              .catch(function(error){
-                  console.error('Error:', error);
-              });
-
-
+                .then(function(response) {
+                    data.industries = response.data.rows;
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                });
         };
 
-        this.getTopics = function() {
+        var getTopics = function() {
             $http.get('/db/topics')
-            .then(function(response){
-                this.data.topics = response.data.rows;
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            });
-
-
+                .then(function(response) {
+                    data.topics = response.data.rows;
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                });
         };
 
-        this.getCountries = function() {
+        var getCountries = function() {
             $http.get('/db/countries')
-            .then(function(response){
-                this.data.countries = response.data.rows;
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            });
-
-
+                .then(function(response) {
+                    data.countries = response.data.rows;
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                });
         };
 
-        /*
-         *  END OF PUBLIC
-         *  START OF PRIVATE (ADMIN)
-         *
-         */
-
-        this.getContacts = function() {
+        var getContacts = function() {
             $http.get('/db/contacts')
-            .then(function(response){
-                this.data.contacts = response.data.rows;
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            });
-
-
+                .then(function(response) {
+                    data.contacts = response.data.rows;
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                });
         };
 
-        this.getWebsites = function() {
+        var getWebsites = function() {
             $http.get('/db/websites')
-            .then(function(response){
-                this.data.websites = response.data.rows;
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            });
-
-
+                .then(function(response) {
+                    data.websites = response.data.rows;
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                });
         };
 
-        this.getUnmatched = function() {
+        var getUnmatched = function() {
             $http.get('/db/unmatched')
-            .then(function(response){
-                this.data.unmatched = response.data.rows;
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            });
-
-
+                .then(function(response) {
+                    data.unmatched = response.data.rows;
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                });
         };
 
-        this.getTopicsNumberOfHits = function() {
+        var getTopicsNumberOfHits = function() {
             $http.get('/db/topics/number_of_hits')
-            .then(function(response){
-                this.data.topicsNumberOfHits = response.data.rows;
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            });
-
-
+                .then(function(response) {
+                    data.topicsNumberOfHits = response.data.rows;
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                });
         };
 
         /*
-         *  END OF PRIVATE (ADMIN)
-         *  END OF GET * CALLS
-         *
+         *  POST CALLS
          */
 
-
-        /*
-         *  START OF POST CALLS
-         *  ADMIN ONLY (EXCEPT FOR 'UNMATCHED')
-         *
-         */
-
-        this.createContact = function(contactObject) {
+        var createContact = function(contactObject) {
             //  contactObject has:
             //  first_name, last_name, title, organization, email, phone
             $http.post('/contacts/create', contactObject)
-            .then(function(response){
-                console.log('Successful POST to /contacts/create');
-                this.getContacts();
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            })
+                .then(function(response) {
+                    console.log('Successful POST to /contacts/create');
+                    getContacts();
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                })
         };
 
-        this.createCountry = function(countryObject) {
+        var createCountry = function(countryObject) {
             //  countryObject has:
             //  contact_id, country
             $http.post('/countries/create', countryObject)
-            .then(function(response){
-                console.log('Successful POST to /countries/create');
-                this.getCountries();
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            })
+                .then(function(response) {
+                    console.log('Successful POST to /countries/create');
+                    var getCountries();
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                })
         };
 
-        this.createIndustry = function(industryObject) {
+        var createIndustry = function(industryObject) {
             //  industryObject has:
             //  industry, note_1, note_2, note_3, contact_1, contact_2,
             //  contact_3, website_1, website_2, website_3
             $http.post('/industries/create', industryObject)
-            .then(function(response){
-                console.log('Successful POST to /industries/create');
-                this.getIndustries();
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            })
+                .then(function(response) {
+                    console.log('Successful POST to /industries/create');
+                    var getIndustries();
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                })
         };
 
-        this.createTopic = function(topicObject) {
+        var createTopic = function(topicObject) {
             //  topicObject has:
             //  topic, note_1, note_2, note_3, contact_1, contact_2,
             //  contact_3, website_1, website_2, website_3
             $http.post('/topics/create', topicObject)
-            .then(function(response){
-                console.log('Successful POST to /topics/create');
-                this.getTopics();
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            })
+                .then(function(response) {
+                    console.log('Successful POST to /topics/create');
+                    var getTopics();
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                })
         };
 
-        this.createWebsite = function(websiteObject) {
+        var createWebsite = function(websiteObject) {
             //  websiteObject has:
             //  website
             $http.post('/websites/create', websiteObject)
-            .then(function(response){
-                console.log('Successful POST to /websites/create');
-                this.getWebsites();
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            })
+                .then(function(response) {
+                    console.log('Successful POST to /websites/create');
+                    var getWebsites();
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                })
         };
 
-        this.createUnmatchedTopic = function(unmatchedTopicObject) {
+        var createUnmatchedTopic = function(unmatchedTopicObject) {
             //  unmatchedTopicObject has:
             //  unmatched_topic
             $http.post('/unmatched/create', unmatchedTopicObject)
-            .then(function(response){
-                console.log('Successful POST to /unmatched/create');
-                this.getWebsites();
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            })
+                .then(function(response) {
+                    console.log('Successful POST to /unmatched/create');
+                    var getWebsites();
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                })
         };
 
         /*
-         *  START OF MANAGER CALLS (FIREBASE)
-         *  ADMIN ONLY
-         *
+         *  FIREBASE CALLS
          */
 
-        //  Non-ideal but probably what we have to do
-        //  All of these methods ONLY operate on the currently logged in user
-        this.createNewUser = function(email, password) {
+        var createNewUser = function(email, password) {
             Auth.$createUserWithEmailAndPassword(email, password)
-            .then(function(user){
-                console.log('User', user.uid, 'created successfully!');
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            });
+                .then(function(user) {
+                    console.log('User', user.uid, 'created successfully!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                });
         };
 
-        this.updateUserPassword = function(authObject, newPassword) {
-          authObject.$updatePassword(newPassword)
-          .then(function() {
-              console.log('Password changed successfully!');
-          }).catch(function(error) {
-              console.error('Error:', error);
-          });
+        var updateUserPassword = function(newPassword) {
+            Auth.$updatePassword(newPassword)
+                .then(function() {
+                    console.log('Password changed successfully!');
+                }).catch(function(error) {
+                    console.error('Error:', error);
+                });
         };
 
-        this.updateUserEmail = function(authObject, newEmail) {
-          authObject.$updateEmail(newEmail)
-          .then(function() {
-              console.log('Email changed successfully!');
-          }).catch(function(error) {
-              console.error('Error:', error);
-          });
+        var updateUserEmail = function(newEmail) {
+            Auth.$updateEmail(newEmail)
+                .then(function() {
+                    console.log('Email changed successfully!');
+                }).catch(function(error) {
+                    console.error('Error:', error);
+                });
         };
 
-        this.deleteUser = function(authObject){
-            authObject.$deleteUser()
-            .then(function() {
-                console.log('User deleted successfully!');
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            })
+        var deleteUser = function() {
+            Auth.$deleteUser()
+                .then(function() {
+                    console.log('User deleted successfully!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                })
         };
 
-        this.sendPasswordResetEmail = function(authObject, email){
-            authObject.$sendPasswordResetEmail(email)
-            .then(function(){
-                console.log('Password reset email sent successfully!');
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            });
+        var sendPasswordResetEmail = function(email) {
+            Auth.$sendPasswordResetEmail(email)
+                .then(function() {
+                    console.log('Password reset email sent successfully!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                });
         };
 
-        //  EXPORT ALL METHODS AND PROPERTIES ATTACHED TO 'THIS'
+        //  FACTORY-EXPORTED FUNCTIONS AND OBJECTS
         return {
-          data: data,
-          getIndustries: getIndustries
+            //  SYNCHRONIZED DATA OBJECT
+            data: data,
+            //  GET CALLS
+            getIndustries: getIndustries,
+            getTopics: getTopics,
+            getCountries: getCountries,
+            getContacts: getContacts,
+            getWebsites: getWebsites,
+            getUnmatched: getUnmatched,
+            getTopicsNumberOfHits: getTopicsNumberOfHits,
+            //  POST CALLS
+            createCountry: createCountry,
+            createTopic: createTopic,
+            createContact: createContact,
+            createIndustry: createIndustry,
+            createWebsite: createWebsite,
+            createUnmatchedTopic: createUnmatchedTopic,
+            //  PUT CALLS (COMING SOON!)
+            //  DELETE CALLS (COMING SOON!)
+            //  FIREBASE CALLS
+            createNewUser: createNewUser,
+            updateUserPassword: updateUserPassword,
+            updateUserEmail: updateUserEmail,
+            deleteUser: deleteUser,
+            sendPasswordResetEmail: sendPasswordResetEmail
         };
     }
 
