f4df8c017252e2b1b9e306b92519436f9e06e365
Anthony Maki <4cm4k1@gmail.com>
2016-08-29 09:53:56 -0500

    Add POST calls to Data.js

--- client/scripts/Data.js
+++ client/scripts/Data.js
@@ -115,6 +115,93 @@
          *
          */
 
+
+        /*
+         *  START OF POST CALLS
+         *  ADMIN ONLY (EXCEPT FOR 'UNMATCHED')
+         *
+         */
+
+        this.createContact = function(contactObject) {
+            //  contactObject has:
+            //  first_name, last_name, title, organization, email, phone
+            $http.post('/contacts/create', contactObject)
+            .then(function(response){
+                console.log('Successful POST to /contacts/create');
+                this.getContacts();
+            })
+            .catch(function(error){
+                console.error('Error:', error);
+            })
+        };
+
+        this.createCountry = function(countryObject) {
+            //  countryObject has:
+            //  contact_id, country
+            $http.post('/countries/create', countryObject)
+            .then(function(response){
+                console.log('Successful POST to /countries/create');
+                this.getCountries();
+            })
+            .catch(function(error){
+                console.error('Error:', error);
+            })
+        };
+
+        this.createIndustry = function(industryObject) {
+            //  industryObject has:
+            //  industry, note_1, note_2, note_3, contact_1, contact_2,
+            //  contact_3, website_1, website_2, website_3
+            $http.post('/industries/create', industryObject)
+            .then(function(response){
+                console.log('Successful POST to /industries/create');
+                this.getIndustries();
+            })
+            .catch(function(error){
+                console.error('Error:', error);
+            })
+        };
+
+        this.createTopic = function(topicObject) {
+            //  topicObject has:
+            //  topic, note_1, note_2, note_3, contact_1, contact_2,
+            //  contact_3, website_1, website_2, website_3
+            $http.post('/topics/create', topicObject)
+            .then(function(response){
+                console.log('Successful POST to /topics/create');
+                this.getTopics();
+            })
+            .catch(function(error){
+                console.error('Error:', error);
+            })
+        };
+
+        this.createWebsite = function(websiteObject) {
+            //  websiteObject has:
+            //  website
+            $http.post('/websites/create', websiteObject)
+            .then(function(response){
+                console.log('Successful POST to /websites/create');
+                this.getWebsites();
+            })
+            .catch(function(error){
+                console.error('Error:', error);
+            })
+        };
+
+        this.createUnmatchedTopic = function(unmatchedTopicObject) {
+            //  unmatchedTopicObject has:
+            //  unmatched_topic
+            $http.post('/unmatched/create', unmatchedTopicObject)
+            .then(function(response){
+                console.log('Successful POST to /unmatched/create');
+                this.getWebsites();
+            })
+            .catch(function(error){
+                console.error('Error:', error);
+            })
+        };
+
         //  EXPORT ALL METHODS AND PROPERTIES ATTACHED TO 'THIS'
         return this;
     }
