01fb2782d333aba671c8fdc166a47a5801401585
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-08-30 16:28:01 -0500

    tweaked and commented results view and controller

--- client/scripts/ResultsCtrl.js
+++ client/scripts/ResultsCtrl.js
@@ -8,40 +8,65 @@
     function ResultsCtrl($location, $routeParams, $http, Data) {
         var vm = this;
         vm.data = Data.data;
-
-        // console.log('industry:', $routeParams.industry);
-        // console.log('topic (optional):', $routeParams.topic);
+        vm.isOther = false;
+        vm.noMatch = false;
+        vm.hasMatch = false;
+        vm.selectedTopic = null;
+        vm.item = undefined;
         vm.industry = $routeParams.industry;
         vm.topic = $routeParams.topic;
+        vm.displaySelectedTopic = function() {
+            // vm.hasMatch = vm.checkHasMatch();
+            checkHasMatch();
+            console.log('hasMatch?', vm.hasMatch);
+            console.log('displaying selected topic', vm.selectedTopic);
+            // return (vm.selectedTopic);
+        };
+        // console.log('industry:', $routeParams.industry);
+        // console.log('topic (optional):', $routeParams.topic);
+        checkIsOther();
+        getIndustryData();
+        getTopicList();
 
-        vm.isOther = function() {
-            if (vm.industry != 7) {
-                return false;
-            } else {
-                return true;
-            }
+
+        Data.getCountries();
+
+
+        // this code needs to go into the function that fires when someone chooses a topic
+
+        //
+        // $http.put('/db/topics/number_of_hits').then(function(response){
+        //         console.log('adding to your hits', response);
+        //     },
+        //     function(response) {
+        //         console.log('error adding to hits', response);
+        //     });
+
+        //check if user selected 'other industry'
+        function checkIsOther(){
+          if(vm.industry != 7){
+            vm.isOther = false;
+          } else {
+            vm.isOther = true
+          }
         };
 
-
-        vm.getIndustryData = function() {
+        //get data for an industry
+        function getIndustryData() {
             var indId = $routeParams.industry;
             indId = parseInt(indId);
             console.log('industries', indId);
             $http.get('/db/industries').then(function(response) {
                     // console.log('getting industries', response.data);
                     vm.industryList = response.data.rows;
-                    return (vm.industryList);
                 },
                 function(response) {
                     console.log('error getting industry data', response.data);
                 });
         };
 
-        vm.getIndustryData();
-
-
         //this is to get the list of Topics which should display only when someone has selected "other industries"
-        vm.getTopicList = function() {
+        function getTopicList() {
             // var topicId = $routeParams.topic.id;
             // topicId = parseInt(topicId);
             // console.log('topics', topicId);
@@ -49,31 +74,13 @@
                     // console.log('getting topics', response.data);
                     vm.topicList = response.data.rows;
                     // console.log('topicList', vm.topicList);
-                    return (vm.topicList);
                 },
                 function(response) {
                     console.log('error getting topic data', response.data);
                 });
-            return (vm.topicList);
         };
-        vm.getTopicList();
-
-        vm.noMatch = false;
-        vm.hasMatch = false;
-        vm.selectedTopic = null;
-        vm.displaySelectedTopic = function() {
-
-            // vm.hasMatch = vm.checkHasMatch();
-            vm.checkHasMatch();
-            console.log('hasMatch?', vm.hasMatch);
-            console.log('displaying selected topic', vm.selectedTopic);
-            // return (vm.selectedTopic);
-
-        };
-
-
-        vm.item = undefined;
-        vm.checkHasMatch = function() {
+        //check if the search topic matches
+        function checkHasMatch() {
             for (var i = 0; i < vm.topicList.length; i++) {
                 if (vm.topicList[i].topic == vm.selectedTopic) {
                     console.log('vm.checkHasMatch found a match in topicList', vm.selectedTopic, vm.topicList[i]);
@@ -89,21 +96,6 @@
                 }
             }
         };
-
-        Data.getCountries();
-
-
-        // this code needs to go into the function that fires when someone chooses a topic
-
-        //
-        // $http.put('/db/topics/number_of_hits').then(function(response){
-        //         console.log('adding to your hits', response);
-        //     },
-        //     function(response) {
-        //         console.log('error adding to hits', response);
-        //     });
-
-
 
     }
 })();
