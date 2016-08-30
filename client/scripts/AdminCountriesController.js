91a44c7194d09b9874ddebeb7bda09ecc973801a
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-08-30 12:08:33 -0500

    admin/countries view started

--- client/scripts/AdminCountriesController.js
+++ client/scripts/AdminCountriesController.js
@@ -3,11 +3,11 @@
 
   angular.module('exportsNavigator').controller('AdminCountriesController', AdminCountriesController);
 
-  AdminCountriesController.$inject = ['currentAuth'];
+  AdminCountriesController.$inject = ['currentAuth', 'Data'];
 
-  function AdminCountriesController(currentAuth) {
+  function AdminCountriesController(currentAuth, Data) {
     var vm = this;
-
-    
+    vm.data = Data.data;
+    Data.getCountries();
   }
 })();
