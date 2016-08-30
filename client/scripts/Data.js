8e84a1249f57a6caddcab24429d05992ef142ba6
Anthony Maki <4cm4k1@gmail.com>
2016-08-30 17:52:01 -0500

    Add showToast() in Firebase call error handling, atom beautify syntax in Data.js

--- client/scripts/Data.js
+++ client/scripts/Data.js
@@ -18,9 +18,9 @@
         var showToast = function(message) {
             $mdToast.show(
                 $mdToast.simple()
-                    .textContent(message)
-                    .position('top right')
-                    .hideDelay(3000)
+                .textContent(message)
+                .position('top right')
+                .hideDelay(3000)
             );
         };
 
@@ -196,171 +196,171 @@
          *  PUT CALLS
          */
 
-        var updateContact = function(contactObject){
-          //  contactObject has:
-          //  id, first_name, last_name, title, organization, email, phone
-          return $http.put('/db/contacts/update', contactObject)
-              .then(function(response) {
-                  console.log('Successful PUT to /db/contacts/update');
-                  getContacts();
-                  showToast('Contact successfully updated!');
-              })
-              .catch(function(error) {
-                  console.error('Error:', error);
-                  showToast('Sorry, updating the contact failed. Please try again.\nError: ' + error);
-              });
+        var updateContact = function(contactObject) {
+            //  contactObject has:
+            //  id, first_name, last_name, title, organization, email, phone
+            return $http.put('/db/contacts/update', contactObject)
+                .then(function(response) {
+                    console.log('Successful PUT to /db/contacts/update');
+                    getContacts();
+                    showToast('Contact successfully updated!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                    showToast('Sorry, updating the contact failed. Please try again.\nError: ' + error);
+                });
         };
 
-        var updateCountry = function(countryObject){
-          //  countryObject has:
-          //  id, contact_id, country
-          return $http.put('/db/countries/update', countryObject)
-              .then(function(response) {
-                  console.log('Successful PUT to /db/countries/update');
-                  getCountries();
-                  showToast('Country successfully updated!');
-              })
-              .catch(function(error) {
-                  console.error('Error:', error);
-                  showToast('Sorry, updating the country failed. Please try again.\nError: ' + error);
-              });
+        var updateCountry = function(countryObject) {
+            //  countryObject has:
+            //  id, contact_id, country
+            return $http.put('/db/countries/update', countryObject)
+                .then(function(response) {
+                    console.log('Successful PUT to /db/countries/update');
+                    getCountries();
+                    showToast('Country successfully updated!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                    showToast('Sorry, updating the country failed. Please try again.\nError: ' + error);
+                });
         };
 
-        var updateIndustry = function(industryObject){
-          //  industryObject has:
-          //  id, industry, note_1, note_2, note_3, contact_1, contact_2, contact_3, website_1, website_2, website_3
-          return $http.put('/db/industries/update', industryObject)
-              .then(function(response) {
-                  console.log('Successful PUT to /db/industries/update');
-                  getIndustries();
-                  showToast('Industry successfully updated!');
-              })
-              .catch(function(error) {
-                  console.error('Error:', error);
-                  showToast('Sorry, updating the industry failed. Please try again.\nError: ' + error);
-              });
+        var updateIndustry = function(industryObject) {
+            //  industryObject has:
+            //  id, industry, note_1, note_2, note_3, contact_1, contact_2, contact_3, website_1, website_2, website_3
+            return $http.put('/db/industries/update', industryObject)
+                .then(function(response) {
+                    console.log('Successful PUT to /db/industries/update');
+                    getIndustries();
+                    showToast('Industry successfully updated!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                    showToast('Sorry, updating the industry failed. Please try again.\nError: ' + error);
+                });
         };
 
-        var updateTopic = function(topicObject){
-          //  topicObject has:
-          //  id, topic, note_1, note_2, note_3, contact_1, contact_2, contact_3, website_1, website_2, website_3
-          return $http.put('/db/topics/update', topicObject)
-              .then(function(response) {
-                  console.log('Successful PUT to /db/topics/update');
-                  getTopics();
-                  showToast('Topic successfully updated!');
-              })
-              .catch(function(error) {
-                  console.error('Error:', error);
-                  showToast('Sorry, updating the topic failed. Please try again.\nError: ' + error);
-              });
+        var updateTopic = function(topicObject) {
+            //  topicObject has:
+            //  id, topic, note_1, note_2, note_3, contact_1, contact_2, contact_3, website_1, website_2, website_3
+            return $http.put('/db/topics/update', topicObject)
+                .then(function(response) {
+                    console.log('Successful PUT to /db/topics/update');
+                    getTopics();
+                    showToast('Topic successfully updated!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                    showToast('Sorry, updating the topic failed. Please try again.\nError: ' + error);
+                });
         };
 
-        var updateTopicNumberOfHits = function(topicIdObject){
-          //  topicIdObject has:
-          //  id
-          return $http.put('/db/topics/update/number_of_hits', topicIdObject)
-              .then(function(response) {
-                  console.log('Successful PUT to /db/topics/update/number_of_hits');
-                  getTopicsNumberOfHits();
-              })
-              .catch(function(error) {
-                  console.error('Error:', error);
-              });
+        var updateTopicNumberOfHits = function(topicIdObject) {
+            //  topicIdObject has:
+            //  id
+            return $http.put('/db/topics/update/number_of_hits', topicIdObject)
+                .then(function(response) {
+                    console.log('Successful PUT to /db/topics/update/number_of_hits');
+                    getTopicsNumberOfHits();
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                });
         };
 
-        var updateWebsite = function(websiteObject){
-          //  websiteObject has:
-          //  id, website
-          return $http.put('/db/websites/update', websiteObject)
-              .then(function(response) {
-                  console.log('Successful PUT to /db/websites/update');
-                  getWebsites();
-                  showToast('Website successfully updated!');
-              })
-              .catch(function(error) {
-                  console.error('Error:', error);
-                  showToast('Sorry, updating the website failed. Please try again.\nError: ' + error);
-              });
+        var updateWebsite = function(websiteObject) {
+            //  websiteObject has:
+            //  id, website
+            return $http.put('/db/websites/update', websiteObject)
+                .then(function(response) {
+                    console.log('Successful PUT to /db/websites/update');
+                    getWebsites();
+                    showToast('Website successfully updated!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                    showToast('Sorry, updating the website failed. Please try again.\nError: ' + error);
+                });
         };
 
         /*
          *  DELETE CALLS
          */
 
-        var deleteContact = function(contactIdObject){
-          //  contactIdObject has:
-          //  id
-          return $http.delete('/db/contacts/delete', contactIdObject)
-              .then(function(response) {
-                  console.log('Successful DELETE to /db/contacts/delete');
-                  getContacts();
-                  showToast('Contact successfully deleted!');
-              })
-              .catch(function(error) {
-                  console.error('Error:', error);
-                  showToast('Sorry, deleting the contact failed. Please try again.\nError: ' + error);
-              });
+        var deleteContact = function(contactIdObject) {
+            //  contactIdObject has:
+            //  id
+            return $http.delete('/db/contacts/delete', contactIdObject)
+                .then(function(response) {
+                    console.log('Successful DELETE to /db/contacts/delete');
+                    getContacts();
+                    showToast('Contact successfully deleted!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                    showToast('Sorry, deleting the contact failed. Please try again.\nError: ' + error);
+                });
         };
 
-        var deleteCountry = function(countryIdObject){
-          //  countryIdObject has:
-          //  id
-          return $http.delete('/db/countries/delete', countryIdObject)
-              .then(function(response) {
-                  console.log('Successful DELETE to /db/countries/delete');
-                  getCountries();
-                  showToast('Country successfully deleted!');
-              })
-              .catch(function(error) {
-                  console.error('Error:', error);
-                  showToast('Sorry, deleting the country failed. Please try again.\nError: ' + error);
-              });
+        var deleteCountry = function(countryIdObject) {
+            //  countryIdObject has:
+            //  id
+            return $http.delete('/db/countries/delete', countryIdObject)
+                .then(function(response) {
+                    console.log('Successful DELETE to /db/countries/delete');
+                    getCountries();
+                    showToast('Country successfully deleted!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                    showToast('Sorry, deleting the country failed. Please try again.\nError: ' + error);
+                });
         };
 
-        var deleteIndustry = function(industryIdObject){
-          //  industryIdObject has:
-          //  id
-          return $http.delete('/db/industries/delete', industryIdObject)
-              .then(function(response) {
-                  console.log('Successful DELETE to /db/industries/delete');
-                  getIndustries();
-                  showToast('Industry successfully deleted!');
-              })
-              .catch(function(error) {
-                  console.error('Error:', error);
-                  showToast('Sorry, deleting the industry failed. Please try again.\nError: ' + error);
-              });
+        var deleteIndustry = function(industryIdObject) {
+            //  industryIdObject has:
+            //  id
+            return $http.delete('/db/industries/delete', industryIdObject)
+                .then(function(response) {
+                    console.log('Successful DELETE to /db/industries/delete');
+                    getIndustries();
+                    showToast('Industry successfully deleted!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                    showToast('Sorry, deleting the industry failed. Please try again.\nError: ' + error);
+                });
         };
 
-        var deleteTopic = function(topicIdObject){
-          //  topicIdObject has:
-          //  id
-          return $http.delete('/db/topics/delete', topicIdObject)
-              .then(function(response) {
-                  console.log('Successful DELETE to /db/topics/delete');
-                  getTopics();
-                  showToast('Topic successfully deleted!');
-              })
-              .catch(function(error) {
-                  console.error('Error:', error);
-                  showToast('Sorry, deleting the topic failed. Please try again.\nError: ' + error);
-              });
+        var deleteTopic = function(topicIdObject) {
+            //  topicIdObject has:
+            //  id
+            return $http.delete('/db/topics/delete', topicIdObject)
+                .then(function(response) {
+                    console.log('Successful DELETE to /db/topics/delete');
+                    getTopics();
+                    showToast('Topic successfully deleted!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                    showToast('Sorry, deleting the topic failed. Please try again.\nError: ' + error);
+                });
         };
 
-        var deleteWebsite = function(websiteIdObject){
-          //  websiteIdObject has:
-          //  id
-          return $http.delete('/db/websites/delete', websiteIdObject)
-              .then(function(response) {
-                  console.log('Successful DELETE to /db/websites/delete');
-                  getWebsites();
-                  showToast('Website successfully deleted!');
-              })
-              .catch(function(error) {
-                  console.error('Error:', error);
-                  showToast('Sorry, deleting the website failed. Please try again.\nError: ' + error);
-              });
+        var deleteWebsite = function(websiteIdObject) {
+            //  websiteIdObject has:
+            //  id
+            return $http.delete('/db/websites/delete', websiteIdObject)
+                .then(function(response) {
+                    console.log('Successful DELETE to /db/websites/delete');
+                    getWebsites();
+                    showToast('Website successfully deleted!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                    showToast('Sorry, deleting the website failed. Please try again.\nError: ' + error);
+                });
         };
 
         /*
@@ -374,6 +374,7 @@
                 })
                 .catch(function(error) {
                     console.error('Error:', error);
+                    showToast('Sorry, creating a new user failed. Please try again.\nError: ' + error);
                 });
         };
 
@@ -383,6 +384,7 @@
                     showToast('Password changed successfully!');
                 }).catch(function(error) {
                     console.error('Error:', error);
+                    showToast('Sorry, changing your password failed. Please try again.\nError: ' + error);
                 });
         };
 
@@ -392,6 +394,7 @@
                     showToast('Email changed to ' + newEmail + ' successfully!');
                 }).catch(function(error) {
                     console.error('Error:', error);
+                    showToast('Sorry, changing your email address failed. Please try again.\nError: ' + error);
                 });
         };
 
@@ -402,6 +405,7 @@
                 })
                 .catch(function(error) {
                     console.error('Error:', error);
+                    showToast('Sorry, deleting the user failed. Please try again.\nError: ' + error);
                 });
         };
 
@@ -412,19 +416,21 @@
                 })
                 .catch(function(error) {
                     console.error('Error:', error);
+                    showToast('Sorry, sending the password reset email failed. Please try again.\nError: ' + error);
                 });
         };
 
         var updateDisplayName = function(displayName) {
-          return Auth.$getAuth().updateProfile({
-            displayName: displayName
-          })
-          .then(function() {
-              showToast('Display name has been set to ' + displayName + ' successfully!');
-          })
-          .catch(function(error){
-              console.error('Error:', error);
-          });
+            return Auth.$getAuth().updateProfile({
+                    displayName: displayName
+                })
+                .then(function() {
+                    showToast('Display name has been set to ' + displayName + ' successfully!');
+                })
+                .catch(function(error) {
+                    console.error('Error:', error);
+                    showToast('Sorry, updating your display name failed. Please try again.\nError: ' + error);
+                });
         };
 
         //  FACTORY-EXPORTED FUNCTIONS AND OBJECTS
