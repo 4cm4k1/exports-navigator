f5f5467996f3f53325241aec41114fbd8f331b95
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-08-30 20:29:50 -0500

    material design

--- client/scripts/ResultsCtrl.js
+++ client/scripts/ResultsCtrl.js
@@ -8,29 +8,27 @@
     function ResultsCtrl($location, $routeParams, $http, Data) {
         var vm = this;
         vm.data = Data.data;
-        vm.isOther = false;
+        vm.showIndustryContacts = false;
+        vm.showTopicSearch = false;
         vm.noMatch = false;
         vm.hasMatch = false;
         vm.selectedTopic = null;
+        vm.selectedCountry = null;
         vm.item = undefined;
         vm.industry = $routeParams.industry;
         vm.topic = $routeParams.topic;
         vm.displaySelectedTopic = function() {
-            // vm.hasMatch = vm.checkHasMatch();
-            checkHasMatch();
-            console.log('hasMatch?', vm.hasMatch);
-            console.log('displaying selected topic', vm.selectedTopic);
-            // return (vm.selectedTopic);
+            checkTopicHasMatch();
+        };
+        vm.displaySelectedCountry = function() {
+            checkCountryHasMatch();
         };
         // console.log('industry:', $routeParams.industry);
         // console.log('topic (optional):', $routeParams.topic);
         checkIsOther();
         getIndustryData();
         getTopicList();
-
-
-        Data.getCountries();
-
+        getCountryList();
 
         // this code needs to go into the function that fires when someone chooses a topic
 
@@ -45,9 +43,11 @@
         //check if user selected 'other industry'
         function checkIsOther(){
           if(vm.industry != 7){
-            vm.isOther = false;
+            vm.showIndustryContacts = true;
+            vm.showTopicSearch = false;
           } else {
-            vm.isOther = true;
+            vm.showIndustryContacts = false;
+            vm.showTopicSearch = true;
           }
         }
 
@@ -55,7 +55,6 @@
         function getIndustryData() {
             var indId = $routeParams.industry;
             indId = parseInt(indId);
-            console.log('industries', indId);
             $http.get('/db/industries').then(function(response) {
                     // console.log('getting industries', response.data);
                     vm.industryList = response.data.rows;
@@ -79,20 +78,47 @@
                     console.log('error getting topic data', response.data);
                 });
         }
+        function getCountryList() {
+            // var topicId = $routeParams.topic.id;
+            // topicId = parseInt(topicId);
+            // console.log('topics', topicId);
+            $http.get('/db/countries').then(function(response) {
+                    // console.log('getting topics', response.data);
+                    vm.countryList = response.data.rows;
+                    // console.log('topicList', vm.topicList);
+                },
+                function(response) {
+                    console.log('error getting country data', response.data);
+                });
+        }
         //check if the search topic matches
-        function checkHasMatch() {
+        function checkTopicHasMatch() {
             for (var i = 0; i < vm.topicList.length; i++) {
                 if (vm.topicList[i].topic == vm.selectedTopic) {
-                    console.log('vm.checkHasMatch found a match in topicList', vm.selectedTopic, vm.topicList[i]);
                     vm.item = vm.topicList[i];
-                    vm.hasMatch = true;
-                    vm.noMatch = false;
+                    vm.hasTopicMatch = true;
+                    vm.noTopicMatch = false;
                     break;
                     // return true;
                 } else {
-                    console.log('vm.checkHasMatch did NOT find a match in topicList', vm.selectedTopic, vm.topicList[i].topic);
-                    vm.hasMatch=false;
-                    vm.noMatch = true;
+                    vm.hasTopicMatch = false;
+                    vm.noTopicMatch = true;
+                }
+            }
+        }
+        //check if the search country matches
+        function checkCountryHasMatch() {
+            for (var i = 0; i < vm.countryList.length; i++) {
+                if (vm.countryList[i].country == vm.selectedCountry) {
+                    vm.item = vm.countryList[i];
+                    vm.hasCountryMatch = true;
+                    vm.noCountryMatch = false;
+                    vm.showTopicSearch = false;
+                    break;
+                    // return true;
+                } else {
+                    vm.hasCountryMatch = false;
+                    vm.noCountryMatch = true;
                 }
             }
         }
