55946732ca05b24b32132550626638eb824e630e
Katie Vogel <katiebvogel@gmail.com>
2016-09-02 17:02:17 -0500

    adminIndustriesEdit only working to DELETE

--- client/scripts/AdminIndustriesController.js
+++ client/scripts/AdminIndustriesController.js
@@ -3,16 +3,34 @@
 
   angular.module('exportsNavigator').controller('AdminIndustriesController', AdminIndustriesController);
 
-  AdminIndustriesController.$inject = ['currentAuth', 'Data'];
+  AdminIndustriesController.$inject = ['currentAuth', '$http', 'Data'];
 
-  function AdminIndustriesController(currentAuth, Data) {
+  function AdminIndustriesController(currentAuth, $http, Data) {
     var vm = this;
 
     vm.data = Data.data;
+
     Data.getIndustries();
 
     vm.add = function() {
       console.log('Add clicked');
     };
-  }
+
+
+  vm.edit = function(){
+    console.log('Edit clicked');
+  };
+
+
+  vm.export = function() {
+    console.log('Export clicked');
+  };
+
+  vm.import = function() {
+    console.log('Import clicked');
+  };
+
+
+}
+
 })();
