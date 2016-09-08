90fb13dd64a89118261db29f5d2931535d279f68
Anthony Maki <4cm4k1@gmail.com>
2016-09-08 18:19:06 -0500

    Remove whitespace in Data.js configRedirect.js now contains functionality to set a firebaseToken header to be sent with every request to protect secure routes on the server adminContacts.html now has 80 percent of parent centered layout on greater than small screen sizes

--- client/scripts/configRedirect.js
+++ client/scripts/configRedirect.js
@@ -1,12 +1,28 @@
 (function() {
     'use strict';
-    angular.module('exportsNavigator').run(['$rootScope', '$location', redirectHome]);
+    angular.module('exportsNavigator').run(['$rootScope', '$location', '$http', 'Auth', redirectHome]);
     //  In combination with the route configuration, this redirects to
     //  the home view if user is not authenticated
-    function redirectHome($rootScope, $location) {
+    function redirectHome($rootScope, $location, $http, Auth) {
         $rootScope.$on('$routeChangeError', function(event, next, previous, error) {
             if (error === 'AUTH_REQUIRED') {
                 $location.path('/');
+            }
+        });
+
+        Auth.$onAuthStateChanged(function(user) {
+            if (user) {
+                user.getToken(true)
+                    .then(function(idToken) {
+                      console.log('idtoken', idToken);
+                        $http.defaults.headers.common.firebaseToken = idToken;
+                        console.log('http default headers:', $http.defaults.headers.common.firebaseToken);
+                    })
+                    .catch(function(error) {
+                        console.log('Didnt work');
+                    });
+            } else {
+                $http.defaults.headers.common.firebaseToken = undefined;
             }
         });
     }
